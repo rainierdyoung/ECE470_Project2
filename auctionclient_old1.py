95914884a596650b93a4d054757d9bc4b2ab4406
rainierdyoung <160531755+rainierdyoung@users.noreply.github.com>
2024-04-08 17:46:44 -0400

    Add files via upload

---
+++ auctionclient_old1.py
@@ -0,0 +1,125 @@
+import requests
+
+def register_user(base_url, username, password, email):
+    data =  {"Username":username, "Password":password, "Email":email}
+    response = requests.post(f"{base_url}/register", json = data)
+    print(response.text)
+    return response.json()
+
+def login_user(base_url, username, password):
+    data = {"Username":username, "Password":password} #package login credentials into dictionary 
+    response = requests.post(f"{base_url}/login", json = data) #sending a POST request to the login server with the credentials
+    if response.status_code == 200: #if login is succesful
+        return response.json()    
+    else:
+        return {"success":False, "msg":"Failed to login, status code: " +str(response.status_code)}
+
+def add_item(base_url, seller_id, item_name, item_desc, start_price, start_date, end_date):
+    data = {"Seller_ID":seller_id, "Item_Name":item_name, "Item_Desc":item_desc, "Start_Price":start_price, "Start_Date":start_date, "End_Date":end_date}
+    response = requests.post(f"{base_url}/add_item", json = data)
+    return response.json()
+
+
+def bid_item(base_url, item_id, bidder_id, bid_amount):
+    data = {"Item_ID":item_id,"Bidder_ID":bidder_id,"Bid_Amount":bid_amount}
+    response = requests.post(f"{base_url}/bid_item", json=data)
+    return response.json
+
+def list_items(base_url):
+    response = requests.get(f"{base_url}/items")
+    return response.json()
+
+def get_results(base_url, item_id):
+    response = requests.get(f"{base_url}/get_results", params={"Item_ID":item_id})
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return {"success":False, "msg":"Failed to retrieve results"}
+
+def check_auction(base_url, item_id):
+    data = {"Item_ID":item_id}
+    response = requests.post(f"{base_url}/check_auction", json = data)
+    if response.status_code == 200:
+        result = response.json()
+        if result.get("success"):
+            if "seller_msg" in result:
+                print(result["seller_msg"])
+            else:
+                print(result["msg"])
+        else:
+            print("Auction check failed!", result.get('msg'))
+    else:
+        print("Failed to check auction end!", response.status_code)
+
+#print(register_user(base_url, "New_User", "New_Pass", "new@gmail.com"))
+
+#print(login_user(base_url, "New_User", "New_Pass"))
+
+#print(list_items(base_url))
+
+#print(add_item(base_url, "7", "Shirt_2", "a black shirt", "1.50", "2024-03-06 00:00:00", "2024-03-10 00:00:00"))
+
+#print(bid_item(base_url, "1", "7", "2.50"))
+
+
+def main_client_logic():
+    base_url = 'http://127.0.0.1:5000'
+    print("Welcome to the Auction Site!")
+    role = input("Do you want to be a seller or a bidder? Enter 'Seller' or 'Bidder':").strip().lower()
+    action = input("Do you want to register or login? Enter 'Register' or 'Login'").strip().lower()
+
+
+    if action == "register": #if user chooses to register then register, else, continue to login
+        username = input("Enter your username: ")
+        password = input("Enter your password: ")
+        email = input("Enter your email address: ")
+        registration_response = register_user(base_url, username, password, email) #registration response
+        print(registration_response)
+    
+    username = input("Enter your username: ") 
+    password = input("Enter your password: ")
+    login_response = login_user(base_url, username, password)
+    print(login_response)
+
+    if login_response.get("success"): #if login response is succesful and user is logged in
+        
+        list = input("Would you like to list items? Enter y or n: ").strip().lower()
+        if list == "y":
+            print("Current Items: ")
+            print(list_items(base_url))  
+
+        chck_auct = input("Would you like to check the results of an auction? Enter y or n: ").strip().lower()
+        if chck_auct == "y":    
+            auction_to_check = input("Enter the id for the item which you would like to check :")
+            check_auction(base_url, auction_to_check)
+         
+
+        check_results = input("Would you like to view the highest bid for an Item? Enter y or n: ").strip().lower()
+        if check_results == "y":
+            item_to_fetch = input("Enter the ID for the item which you would like to check :")
+            ch_results = get_results(base_url, item_to_fetch)
+            print("Auction Results: ")
+            print(ch_results)
+
+        if role == "seller": #selling procedure
+            item_name = input("Enter the title of item you would like to sell: ")
+            item_desc = input("Enter a description for your item: ")
+            start_price = input("Enter the starting price for your item: ")
+            start_date = input("Enter a start date of auction: YYYY-MM-DD: ")
+            end_date = input("Enter a end date for the auction: YYYY-MM-DD: ")
+            seller_id = login_response.get("user",{}).get("User_ID")
+            add_item_response = add_item(base_url, seller_id, item_name, item_desc, start_price, start_date, end_date)
+            print(add_item_response)
+        elif role == "bidder": #bidding procedure   
+            item_id = input("Which item would you like to bid on? Enter the item ID #: ")
+            bid_amount = input("How much would you like to bid?: ")
+            bidder_id = login_response.get("user",{}).get("User_ID")
+            bid_item_response = bid_item(base_url, item_id, bidder_id, bid_amount)
+            print(bid_item_response)
+        else:
+            print("Invalid role selected!")
+        
+        
+
+main_client_logic()
+
---
+++ auctionsite_old1.py
@@ -0,0 +1,270 @@
+from flask import Flask, request, jsonify
+import mysql.connector 
+from mysql.connector import Error, pooling
+from flask_apscheduler import APScheduler
+from flask_mail import Mail, Message
+app = Flask(__name__) #initialize a new flask application
+scheduler = APScheduler()
+scheduler.init_app(app)
+scheduler.start()
+
+
+
+try: #initialize pool 
+    pool = mysql.connector.pooling.MySQLConnectionPool(
+        pool_name= 'my_pool',
+        pool_size= 5,
+        host = 'localhost',
+        user = 'root',
+        password = 'Pa@sspass19',
+        database = 'online_auction'
+        )
+except Error as e: #if error occurs during connection
+    print('Error creating a conneciton pool',e)
+
+
+
+mail_settings = {
+    "MAIL_SERVER":'smtp.gmail.com',
+    "MAIL_PORT":587,
+    "MAIL_USE_TLS": True,
+    "MAIL_USE_SSL": False,
+    "MAIL_USERNAME": '470auctionsite@gmail.com',
+    "MAIL_PASSWORD": '470_Auction_24!'
+} 
+app.config.update(mail_settings)
+mail = Mail(app)
+
+def send_email(subject, recipients, body):
+    with app.app_context():
+        msg = Message(subject, sender=app.config.get("MAIL_USERNAME"), recipients=[recipients])
+        msg.body = body
+        mail.send(msg)
+
+
+def process_auction_end(item_id):
+    connection = create_db_connection()
+    cursor = connection.cursor(dictionary=True) # fetching bidder information
+    try:
+        #cursor.execute(""" 
+         #           SELECT Bidder_ID, MAX(Bid_Amount) AS Highest_Bid, 
+          #          FROM bids 
+           #         WHERE Item_ID = %s
+            #        GROUP BY Bidder_ID
+             #       ORDER BY Highest_Bid DESC
+              #      LIMIT 1""",
+               #     (item_id,))
+        query = """ SELECT Bidder_ID, MAX(Bid_Amount) AS Highest_Bid, 
+                    FROM bids 
+                    WHERE Item_ID = %s
+                    GROUP BY Bidder_ID
+                    ORDER BY Highest_Bid DESC
+                    LIMIT 1"""
+        print("query: ", query, "item_id :", item_id)
+        cursor.execute(query, (item_id,))
+        highest_bid = cursor.fetchone()  
+        if highest_bid:
+            cursor.execute('SELECT Email FROM users WHERE User_ID = %s', (highest_bid['Bidder_ID'],))
+            winner_info = cursor.fetchone()
+            if winner_info:
+                send_email('Auction Won', [winner_info['Email']], 'Congratualtions, You won the auction!')
+
+            cursor.execute("SELECT Seller_ID FROM items WHERE Item_ID = %s", (item_id,))  #fetching seller information
+            item_info = cursor.fetchone()
+            if item_info:
+                cursor.execute('SELECT Email FROM users WHERE User_ID = %s', (item_info['Seller_ID'],))
+                seller_info = cursor.fetchone()
+                if seller_info:
+                    send_email("Your Auction has ended", [seller_info['Email']], "Your auction has ended and your item was sold.")
+    except Error as e:
+        print('Error while processing auction end',str(e)) 
+    finally:
+        cursor.close()
+        connection.close()
+
+
+@scheduler.task('interval', id = 'check_auctions', seconds = 10, misfire_grace_time=900)
+def check_auctions():
+    connection=None
+    try:
+        connection = create_db_connection()
+        if connection is not None:
+            cursor = connection.cursor(dictionary=True)
+            cursor.execute("SELECT * FROM items WHERE End_Date <= NOW()")
+            auctions = cursor.fetchall()
+            for auction in auctions:
+                process_auction_end(auction['Item_ID'])
+        else:
+            print("Failed to connect.") 
+    except Error as e:
+        print('An error occured: ',e)
+    finally:
+        cursor.close()
+        connection.close()
+
+
+def create_db_connection(): #create a database connection
+    connection =  None #initialize connection variable
+    try:
+        connection = pool.get_connection()
+    except Error as e: #if error occurs during connection
+        print('The error occured: ',e)
+    return connection
+
+
+
+@app.route('/items', methods = ['GET'])
+def list_items():
+    connection = create_db_connection()
+    cursor = connection.cursor(dictionary=True)
+    cursor.execute("SELECT * FROM items")
+    items = cursor.fetchall()
+    cursor.close()
+    connection.close()
+    return jsonify(items)
+
+@app.route('/get_results', methods = ['GET'])
+def get_results():
+    item_id = request.args.get("Item_ID")
+    connection = create_db_connection()
+    if connection is not None:
+        cursor = connection.cursor(dictionary=True)
+        get_bid_sql = """SELECT bids.Bid_ID, bids.Item_ID, bids.Bidder_ID, bids.Bid_Amount, bids.Bid_Time, users.Username AS Bidder_Username, items.Seller_ID 
+        FROM bids 
+        INNER JOIN users ON bids.Bidder_ID = users.User_ID 
+        INNER JOIN items ON bids.Item_ID = items.Item_ID
+        WHERE bids.Item_ID = %s 
+        ORDER BY bids.Bid_Amount DESC 
+        LIMIT 1"""
+        cursor.execute(get_bid_sql, (item_id,)) #execute sql query to get bid information for given item id
+        bid_info = cursor.fetchone() #fetch the highest bid for the item
+        if bid_info:
+            get_seller_sql = "SELECT Username FROM users WHERE User_ID = %s" #query to get sellers username
+            cursor.execute(get_seller_sql, (bid_info['Seller_ID'],)) #executes query with seller id from bid info
+            seller_info = cursor.fetchone() #fetch the sellers information
+            bid_info['Seller_Username'] = seller_info['Username'] if seller_info else 'Unknown' #add sellers username to bid info or mark as unknown
+            cursor.close()
+            connection.close()
+            return jsonify({"success":True, "result":bid_info}),200
+        else:
+            cursor.close()
+            connection.close()
+            return jsonify({"success":False, "msg":"No bids found for this item, or item doesnt exist!"})
+    else:
+        return jsonify({"success":False, "msg":"Database connection failed!"})
+    
+          
+
+
+@app.route('/bid_item', methods = ['POST'])
+def bid_item():
+    bid_details = request.json #get bid details from json body request
+    connection = create_db_connection()
+    if connection is not None:
+        try:
+            cursor = connection.cursor(dictionary=True)
+            add_bid_sql = "INSERT INTO bids(Item_ID, Bidder_ID, Bid_Amount, Bid_Time) VALUES (%s, %s, %s, NOW())" #sql query to insert bid
+            cursor.execute(add_bid_sql, (bid_details['Item_ID'], bid_details['Bidder_ID'], bid_details['Bid_Amount']))
+            connection.commit()
+            return jsonify({"success":True, "msg":"Bid placed succesfully!"}),200 #return success message with status code 200
+        except Error as e:
+            return jsonify({"success":False, "msg":str(e)}),401 #return error message with status code 401
+        finally:
+            cursor.close()
+            connection.close()
+    else:
+        return jsonify({"success":False, "msg":"Database connection failed"}),500 #retrun error message with status code 500
+
+
+@app.route('/check_auction', methods = ['POST'])
+def check_auction():
+    data = request.json
+    item_id = data.get('Item_ID')
+    connection = create_db_connection()
+    if connection is not None:
+        try:
+            cursor = connection.cursor(dictionary=True)
+            cursor.execute("""SELECT Item_ID, Seller_ID, Item_Name, 
+                           End_Date FROM items WHERE Item_ID = %s AND End_Date < NOW()""", (item_id,))
+            auction = cursor.fetchone()
+            if auction:
+                cursor.execute("""SELECT Bidder_ID, MAX(Bid_Amount) 
+                               AS Highest_Bid FROM bids WHERE Item_ID = %s GROUP BY Bidder_ID, Item_ID""", (item_id,))
+                highest_bid = cursor.fetchone()
+                if highest_bid:
+                    seller_msg = f"Your auction for '{auction['Item_Name']}' has ended. Highest bid: {highest_bid['Highest_Bid']}"
+                    winner_msg = f"Congratulations! You won the auction for '{auction['Item_Name']}' with a bid of {highest_bid['Highest_Bid']}"
+                    return jsonify({"success":True, "seller_msg":seller_msg}),200
+                else:
+                    return jsonify({"success":True, "msg":"Auction ended, but no bids were placed!"}),200
+            else:
+                return jsonify({"success":False, "msg":"Auction has not ended, or doesnt exist!"}),400
+        finally:
+            cursor.close()
+            connection.close()
+    else:
+        return jsonify({"success":False, "msg":"Database connection failed"}),500
+
+@app.route('/add_item', methods = ['POST'])
+def add_item():
+    item_details = request.json #get item details from json body of request
+    connection = create_db_connection()
+    if connection is not None:
+        try:
+            cursor = connection.cursor(dictionary=True)
+            add_item_sql = "INSERT INTO items(Seller_ID, Item_Name, Item_Desc, Start_Price, Start_Date, End_Date) VALUES (%s, %s, %s, %s, %s, %s)" #sql query to insert item
+            cursor.execute(add_item_sql, (item_details['Seller_ID'], item_details['Item_Name'], item_details['Item_Desc'], item_details['Start_Price'], item_details['Start_Date'], item_details['End_Date']))
+            connection.commit()
+            return jsonify({"success":True, "msg":"Item added succesfully!"}),200 #return success message with status code 200
+        except Error as e:
+            return jsonify({"success":False, "msg":str(e)}),401 #return error message with status code 401
+        finally:
+            cursor.close()
+            connection.close()
+    else:
+        return jsonify({"success":False, "msg":"Database connection failed"}),500 #retrun error message with status code 500
+
+
+@app.route('/register', methods = ['POST']) #defining a route for a user registration with POST method
+def register_user(): 
+    user_details = request.json #Get the JSON data sent with the request
+    connection = create_db_connection()
+    cursor = connection.cursor()
+    insert_user_sql = "INSERT INTO users(Username, Password, Email, Creation_Date) VALUES (%s, %s, %s, NOW())" #sql query to insert user
+    try:
+        cursor.execute(insert_user_sql, (user_details['Username'], user_details['Password'], user_details['Email'])) #execute the query with data
+        connection.commit() #commit the transaction
+        return jsonify({"success":True, "msg":"User registered succesfully!"}),200 #return success message with status code 200
+    except Error as e:
+        return jsonify({"success":False, "msg":str(e)}),401 #return error message with status code 401
+    finally:
+        cursor.close()
+        connection.close()
+
+@app.route('/login', methods = ['POST']) #defining a route for user login with post method
+def login_user():
+    login_details = request.json #get login details from json body of request
+    connection = create_db_connection() #create a new database connection
+    if connection is not None:
+        cursor = connection.cursor(dictionary=True)
+        find_user_sql = "SELECT * FROM users WHERE Username = %s AND Password = %s" #sql query to find user by username and password
+        try:    
+            cursor.execute(find_user_sql, (login_details['Username'], login_details['Password'])) #execute query with provided login details
+            user = cursor.fetchone() #getting one record from query result
+            cursor.fetchall()
+            if user: #if user was found
+                return jsonify({"success": True, "msg":"Login succesful!", "user":user}),200
+            else:
+                return jsonify({"success":False, "msg":"Invalid username or password", "user":user}),200                
+        except Error as e:
+            return jsonify({"success":False, "msg": str(e), "user":user}),401
+        finally:
+            cursor.close()
+            connection.close()
+    else:
+            return jsonify({"success":False, 'msg':"Database connection failed!"}),500
+    
+
+if __name__ == '__main__':
+    app.run(debug=True, threaded = True)
+
